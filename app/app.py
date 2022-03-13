c4942e40c52d75aa26becffd417ed87ca07b96ee
Gio845 <yovi8456@gmail.com>
2022-03-13 17:45:59 -0600

    Mejoras del sidebar

--- app/app.py
+++ app/app.py
@@ -5,10 +5,6 @@ from app.routes.routeslogin import routes as loginRoutes
 
 app=Flask(__name__)
 
-# app.config['SQlALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/tanquesoxigenos_database'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# SQLAlchemy(app)
-
 
 #PRIMER SEÑORA - Si vas a trabajar en la primera señora, descomenta esto estas rutas y comentas las de la segunda señora
 app.add_url_rule(primerModuloRoutes["index_route"], view_func=primerModuloRoutes['index_controller'])
@@ -21,16 +17,16 @@ app.add_url_rule(primerModuloRoutes["reporteorden_route"], view_func=primerModul
 
 
 #SEGUNDA SEÑORA - Si vas a trabajar en la segunda señora, descomenta estas rutas y comenta las de la primera señora
-# app.add_url_rule(segundoModuloRoutes["index_route"], view_func=segundoModuloRoutes['index_controller'])
-# app.add_url_rule(segundoModuloRoutes["recepcion_route"], view_func=segundoModuloRoutes['recepcion_controller'])
-# app.add_url_rule(segundoModuloRoutes["nuevarecepcion_route"], view_func=segundoModuloRoutes['nuevarecepcion_controller'])
-# app.add_url_rule(segundoModuloRoutes["reporterecepcion_route"], view_func=segundoModuloRoutes['reporterecepcion_controller'])
+#app.add_url_rule(segundoModuloRoutes["index_route"], view_func=segundoModuloRoutes['index_controller'])
+#app.add_url_rule(segundoModuloRoutes["recepcion_route"], view_func=segundoModuloRoutes['recepcion_controller'])
+#app.add_url_rule(segundoModuloRoutes["nuevarecepcion_route"], view_func=segundoModuloRoutes['nuevarecepcion_controller'])
+#app.add_url_rule(segundoModuloRoutes["reporterecepcion_route"], view_func=segundoModuloRoutes['reporterecepcion_controller'])
 
 
 
 #Login o archivos que no estan en un modulo en concreto
-app.add_url_rule(loginRoutes["login_route"], view_func=loginRoutes['login_controller'])
-app.add_url_rule(loginRoutes["registrar_route"], view_func=loginRoutes['registrar_controller'])
+#app.add_url_rule(loginRoutes["login_route"], view_func=loginRoutes['login_controller'])
+#app.add_url_rule(loginRoutes["registrar_route"], view_func=loginRoutes['registrar_controller'])
 
 
 
