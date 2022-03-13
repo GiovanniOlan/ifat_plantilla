8eb9eefb8f5ee914d19c56b9bd367da52ea6554f
lpalacio <luispalacio.ap@gmail.com>
2022-03-13 00:54:25 -0600

    recepcion

--- app/controllers/controllersegundomodulo.py
+++ app/controllers/controllersegundomodulo.py
@@ -7,3 +7,15 @@ from flask import render_template
 class IndexController(MethodView):
     def get(self):
         return render_template('/segundoModulo/index.html')
+
+class RecepcionController(MethodView):
+    def get(self):
+        return render_template('/segundoModulo/recepcion.html')
+
+class NuevarecepcionController(MethodView):
+    def get(self):
+        return render_template('/segundoModulo/nueva-recepcion.html')
+
+class ReporterecepcionController(MethodView):
+    def get(self):
+        return render_template('/segundoModulo/reporte-recepcion.html')
--- app/routes/routessegundomodulo.py
+++ app/routes/routessegundomodulo.py
@@ -4,4 +4,7 @@ from app.controllers.controllersegundomodulo import *
 
 routes = {
     "index_route": "/", "index_controller": IndexController.as_view("index"),
+    "recepcion_route": "/recepcion", "recepcion_controller": RecepcionController.as_view("recepcion"),
+    "nuevarecepcion_route": "/nueva-recepcion", "nuevarecepcion_controller": NuevarecepcionController.as_view("nueva-recepcion"),
+    "reporterecepcion_route": "/reporte-recepcion", "reporterecepcion_controller": ReporterecepcionController.as_view("reporte-recepcion"),
 }
