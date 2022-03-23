8c011400ac8d4e233f049bb388915846c47db49b
lpalacio <luispalacio.ap@gmail.com>
2022-03-23 16:43:32 -0600

    recibo de mercancia

--- app/app.py
+++ app/app.py
@@ -7,20 +7,20 @@ app=Flask(__name__)
 
 
 #PRIMER SEÑORA - Si vas a trabajar en la primera señora, descomenta esto estas rutas y comentas las de la segunda señora
-app.add_url_rule(primerModuloRoutes["index_route"], view_func=primerModuloRoutes['index_controller'])
-app.add_url_rule(primerModuloRoutes["productos_route"], view_func=primerModuloRoutes['productos_controller'])
-app.add_url_rule(primerModuloRoutes["agregarproducto_route"], view_func=primerModuloRoutes['agregarproducto_controller'])
-app.add_url_rule(primerModuloRoutes["nuevaorden_route"], view_func=primerModuloRoutes['nuevaorden_controller'])
-app.add_url_rule(primerModuloRoutes["ordencompras_route"], view_func=primerModuloRoutes['ordencompras_controller'])
-app.add_url_rule(primerModuloRoutes["reporteorden_route"], view_func=primerModuloRoutes['reporteorden_controller'])
+# app.add_url_rule(primerModuloRoutes["index_route"], view_func=primerModuloRoutes['index_controller'])
+# app.add_url_rule(primerModuloRoutes["productos_route"], view_func=primerModuloRoutes['productos_controller'])
+# app.add_url_rule(primerModuloRoutes["agregarproducto_route"], view_func=primerModuloRoutes['agregarproducto_controller'])
+# app.add_url_rule(primerModuloRoutes["nuevaorden_route"], view_func=primerModuloRoutes['nuevaorden_controller'])
+# app.add_url_rule(primerModuloRoutes["ordencompras_route"], view_func=primerModuloRoutes['ordencompras_controller'])
+# app.add_url_rule(primerModuloRoutes["reporteorden_route"], view_func=primerModuloRoutes['reporteorden_controller'])
 
 
 
 #SEGUNDA SEÑORA - Si vas a trabajar en la segunda señora, descomenta estas rutas y comenta las de la primera señora
-#app.add_url_rule(segundoModuloRoutes["index_route"], view_func=segundoModuloRoutes['index_controller'])
-#app.add_url_rule(segundoModuloRoutes["recepcion_route"], view_func=segundoModuloRoutes['recepcion_controller'])
-#app.add_url_rule(segundoModuloRoutes["nuevarecepcion_route"], view_func=segundoModuloRoutes['nuevarecepcion_controller'])
-#app.add_url_rule(segundoModuloRoutes["reporterecepcion_route"], view_func=segundoModuloRoutes['reporterecepcion_controller'])
+app.add_url_rule(segundoModuloRoutes["index_route"], view_func=segundoModuloRoutes['index_controller'])
+app.add_url_rule(segundoModuloRoutes["recibomercancia_route"], view_func=segundoModuloRoutes['recibomercancia_controller'])
+app.add_url_rule(segundoModuloRoutes["nuevorecibo_route"], view_func=segundoModuloRoutes['nuevorecibo_controller'])
+app.add_url_rule(segundoModuloRoutes["reporterecibo_route"], view_func=segundoModuloRoutes['reporterecibo_controller'])
 
 
 
--- app/routes/routessegundomodulo.py
+++ app/routes/routessegundomodulo.py
@@ -4,7 +4,7 @@ from app.controllers.controllersegundomodulo import *
 
 routes = {
     "index_route": "/", "index_controller": IndexController.as_view("index"),
-    "recepcion_route": "/recepcion", "recepcion_controller": RecepcionController.as_view("recepcion"),
-    "nuevarecepcion_route": "/nueva-recepcion", "nuevarecepcion_controller": NuevarecepcionController.as_view("nueva-recepcion"),
-    "reporterecepcion_route": "/reporte-recepcion", "reporterecepcion_controller": ReporterecepcionController.as_view("reporte-recepcion"),
+    "recibomercancia_route": "/recibo-mercancia", "recibomercancia_controller": RecibomercanciaController.as_view("recibo-mercancia"),
+    "nuevorecibo_route": "/nuevo-recibo", "nuevorecibo_controller": NuevoreciboController.as_view("nuevo-recibo"),
+    "reporterecibo_route": "/reporte-recibo", "reporterecibo_controller": ReportereciboController.as_view("reporte-recibo"),
 }
